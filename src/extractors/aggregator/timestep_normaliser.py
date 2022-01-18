9a6c1ff880b6110b2d99626cc3e5334ffbb394db
Jadouille <jade.cock@hotmail.com>
2022-01-18 11:26:12 +0100

    timestep normaliser without the states

--- src/extractors/aggregator/timestep_normaliser.py
+++ src/extractors/aggregator/timestep_normaliser.py
@@ -3,15 +3,18 @@ import numpy as np
 import pandas as pd
 
 from extractors.aggregator.aggregator import Aggregator
+from extractors.sequencer.sequencing import Sequencing
 
 class TimestepNormaliser(Aggregator):
     """This class leaves the sequence as is
     """
     
-    def __init__(self):
+    def __init__(self, sequencer:Sequencing):
         super().__init__()
         self._name = 'timestepnormalised aggregator'
         self._notation = 'tsnormagg'
+        self._sequencer = sequencer
+        self._state_size = self._sequencer.get_vector_states()
 
     def normalise_ts(self, vector):
         if np.sum(vector) == 0:
@@ -20,4 +23,10 @@ class TimestepNormaliser(Aggregator):
         return vec
         
     def aggregate(self, matrix: list) -> list:
-        return [self.normalise_ts(ts) for ts in matrix]
+        labels = []
+        for row in matrix:
+            state = [row[0:self._state_size]]
+            action = self.normalise_ts(row[self._state_size:])
+            labels.append(state + action)
+
+        return labels
