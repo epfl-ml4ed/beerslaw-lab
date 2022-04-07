517784de822988863e295dd2e967667e859b73d5
Jadouille <jade.cock@hotmail.com>
2022-04-07 17:11:08 +0200

    refactoring xval + gridsearches

--- src/ml/scorers/fairness_scorer.py
+++ src/ml/scorers/fairness_scorer.py
@@ -43,78 +43,18 @@ class FairnessScorer(Scorer):
     def _load_id_dictionary(self):
         """Loads the summaries of sequences and information about the students
         """
-        print('loading')
-        path = self._settings['path']['sequenced_simulations'] + self._settings['experiment']['sequencer'] + '/'
-        path += 'id_dictionary.pkl'
-        with open(path, 'rb') as fp:
-            self._id_dictionary = pickle.load(fp)
+        raise NotImplementedError
             
     def _load_demographics(self):
         """Returns the demographics 
         """
-        index = {v:k for (k, v) in self._id_dictionary['index'].items()}
-        with open('//ic1files.epfl.ch/D-VET/Projects/ChemLab/04_Processing/Processing/Data/PostTest/post_test.pkl', 'rb') as fp:
-            post_test = pickle.load(fp)
-            ranks = pd.DataFrame()
-            ranks['lid'] = post_test[0, 'username']
-            ranks['gender'] = post_test[0, 'gender']
-            ranks['year'] = post_test[0, 'year']
-            ranks['field'] = post_test[0, 'field']
-            ranks['ranks'] = post_test[6, 'ranks']
-            
-            ranks = ranks[ranks['ranks'].notna()]
-            ranks['permutation'] = ranks['ranks'].apply(lambda x: ''.join([str(r) for r in x]))
-            ranks = ranks.set_index('lid')
-        
-        
-        self._demographics_map = {}
-        for iid in self._id_dictionary['sequences']:
-            lid = index[iid]
-            self._demographics_map[iid] = {
-                'gender': ranks.loc[lid]['gender'],
-                'year': ranks.loc[lid]['year'],
-                'field': ranks.loc[lid]['field'],
-                'permutation': ranks.loc[lid]['permutation']
-            }
+        raise NotImplementedError
         
     def _get_stratified_scores(self, preds_df: pd.DataFrame, stratifier:str) -> dict:
-        results = {}
-        for strat in preds_df[stratifier].unique():
-            if pd.isnull(strat):
-                continue
-            sdf = preds_df[preds_df[stratifier] == strat]
-            y_true = list(sdf['truth'])
-            y_preds = list(sdf['preds'])
-            y_probs = list(sdf['proba_vector'])
-            results[strat] = self._scorer.get_scores(y_true, y_preds, y_probs)
-        return results
+        raise NotImplementedError
         
     def get_scores(self, new_predictions: dict) -> dict:
-        preds = {}
-        for iid in new_predictions:
-            preds[iid] = {
-                'preds': new_predictions[iid]['predicted_label'],
-                'truth': new_predictions[iid]['ground_truth'],
-                'proba': new_predictions[iid]['proba'],
-                'proba_vector': new_predictions[iid]['prob_vector'],
-                'gender': self._demographics_map[iid]['gender'],
-                'year': self._demographics_map[iid]['year'],
-                'field': self._demographics_map[iid]['field'],
-                'no_strat': 'nothing'
-            }
-        preds = pd.DataFrame(preds).transpose().reset_index().rename(columns={'index': 'lid'})
-        y_true = list(preds['truth'])
-        y_preds = list(preds['preds'])
-        y_probs = list(preds['proba_vector'])
-        print(y_preds)
-        
-        results = {}
-        for strat in self._settings['scorer']['stratifiers']:
-            if strat == 'none':
-                results['overall'] = self._scorer.get_scores(y_true, y_preds, y_probs)
-            else:
-                results.update(self._get_stratified_scores(preds, strat))
-        return results
+        raise NotImplementedError
                  
                 
         
